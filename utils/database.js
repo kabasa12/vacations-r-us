2920226a930030a8ccda91a9f83916bf0126dd5b
kabasa12 <kabasa12@gmail.com>
2020-10-13 23:33:29 +0300

    changed url connection

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql2');
-var CLEARDB_DATABASE_URL = "mysql2://adffdadf2341:adf4234@us-cdbr-east.cleardb.com/heroku_db?reconnect=true"
+var CLEARDB_DATABASE_URL = "mysql2://b605f8a24cff8a:45ccbac3@us-cdbr-east-02.cleardb.com/heroku_8b29697fa2873d3?reconnect=true"
 var con = mysql.createConnection(
     CLEARDB_DATABASE_URL 
 //     {
