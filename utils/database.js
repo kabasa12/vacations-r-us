f6272c4cad05cdcbf824506565d577660e6af2dd
kabasa12 <kabasa12@gmail.com>
2020-10-14 15:19:29 +0300

    change database URL

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,12 @@
 var mysql = require('mysql2');
-var CLEARDB_DATABASE_URL = "mysql2://b605f8a24cff8a:45ccbac3@us-cdbr-east-02.cleardb.com/heroku_8b29697fa2873d3?reconnect=true"
+//var CLEARDB_DATABASE_URL = "mysql2://b605f8a24cff8a:45ccbac3@us-cdbr-east-02.cleardb.com/heroku_8b29697fa2873d3?reconnect=true"
 var con = mysql.createConnection(
-    CLEARDB_DATABASE_URL 
+    {
+        host:"us-cdbr-east-02.cleardb.com",
+        user:"b605f8a24cff8a",
+        password:"45ccbac3",
+        database:"heroku_8b29697fa2873d3"
+    }
 //     {
 //     host: "localhost",
 //     user: "root",
